17a099bd8b68f72ac591b8e9d7423ad9ae4b1aaf
Nafees Bin Zaman <zamannafees@gmai.com>
2022-06-11 01:11:46 +0200

    recursive done

---
+++ Python/Recursion/power.py
@@ -0,0 +1,11 @@
+def power(base, exp):
+
+    assert exp >= 0 and int(exp) == exp, 'The given number must be a non negative integer!' #edge case
+
+    if exp == 0:
+        return 1
+    elif exp == 1:
+        return base
+    return base * power(base, exp-1)
+    
+print(power(2,2))
